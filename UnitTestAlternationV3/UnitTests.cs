4f98a0119a4d74cc292cba6b13df17d96bee685f
Hapro Bishop <acfowo45@gmail.com>
2022-10-06 18:10:36 +0300

    Improve Test

--- UnitTestAlternationV3/UnitTests.cs
+++ UnitTestAlternationV3/UnitTests.cs
@@ -10,7 +10,25 @@ namespace UnitTestAlternationV3
         public void TestLibrary()
         {
             double[] mas = { 3.4, -2.3, 2.4 };
-            Assert.IsTrue(AlternationV3_Array.AlternationV3.ElementAlternationCheck(mas));
+            Assert.IsTrue(AlternationV3_Array.AlternationV3.ElementAlternationCheck(mas));//Must true
+        }
+        [TestMethod]
+        public void TestLibrary2()
+        {
+            double[] mas2 = { -3.4, 2.3, -2.4 };
+            Assert.IsTrue(AlternationV3_Array.AlternationV3.ElementAlternationCheck(mas2));//Must true
+        }
+        [TestMethod]
+        public void TestLibrary3()
+        {
+            double[] mas3 = { 3.4, 2.3, -2.4 };
+            Assert.IsTrue(AlternationV3_Array.AlternationV3.ElementAlternationCheck(mas3));//Must false
+        }
+        [TestMethod]
+        public void TestLibrary4()
+        {
+            double[] mas4 = { -3.4, -2.3, 2.4 };
+            Assert.IsTrue(AlternationV3_Array.AlternationV3.ElementAlternationCheck(mas4));//Must false
         }
     }
 }
